74357f4c2cade9a4632cbd26c50f862a4b6d7a7b
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-21 21:17:03 +0300

    Completed on Aug 21

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(someArray, someString) {
+    return someArray.filter(function (driver) { return driver.toLowerCase() === someString.toLowerCase() })
+  }
+  
+function fuzzyMatch(someArray, someString) {
+    return someArray.filter(function (driver) { return driver.toLowerCase().substring(0, someString.length) === someString.toLowerCase() })
+}
+  
+function matchName(someArray, someString) {
+    return someArray.filter(function (driver) { return driver.name === someString })
+}
+
+
